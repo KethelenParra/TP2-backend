882b30a5ff89fc71bffc08f6059b32704e19b382
Hugo Valuar <hugovaluarb03@gmail.com>
2024-10-07 18:49:29 -0300

    modificacoes box resource

--- src/main/java/br/unitins/topicos1/resource/BoxResource.java
+++ src/main/java/br/unitins/topicos1/resource/BoxResource.java
@@ -19,7 +19,7 @@ import jakarta.ws.rs.core.Response.Status;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@Path("/box")
+@Path("/boxes")
 public class BoxResource {
     
     @Inject
